4d8b339856f571318a90fac47edfb0fb79fce46c
ultimate-yasser <ultimateyasser@gmail.com>
2024-08-05 18:12:14 +0300

    Finished Week one Assignments

---
+++ Week_01/assign_01/index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This is the default file which will open even if
+ * no file is specified in the folder assgin_01
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="<?="UTF-8"?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?="My First PHP Page"?></title>
+</head>
+
+<body>
+    <div><?php echo "We Love";?></div>
+    <div><?php ECHO "Elzero Channel";?></div>
+</body>
+
+</html>
